a1be057f144fa549d2e497a9f033520d8fa6b8e9
Sakthi-kc <kcsakthi17@gmail.com>
2026-01-23 12:29:12 +0530

    Modified the file name

---
+++ get-partner-details/Services/IExcelService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace get_partner_details.Services
+{
+    public interface IExcelService
+    {
+        List<List<string>> GetPartnerData();
+    }
+}
