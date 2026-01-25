ea60e2aca9478e580f8422e2932365b7cdc19690
Sakthi-kc <kcsakthi17@gmail.com>
2026-01-25 17:14:41 +0530

    Corrected notes

--- get-partner-details/obj/Debug/net10.0/get-partner-details.AssemblyInfo.cs
+++ get-partner-details/obj/Debug/net10.0/get-partner-details.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("get-partner-details")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d996c23373de86f36dd004daf59784c4e2ea3ee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0563d007655fe3866099185ea05a3e3bcb547802")]
 [assembly: System.Reflection.AssemblyProductAttribute("get-partner-details")]
 [assembly: System.Reflection.AssemblyTitleAttribute("get-partner-details")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
